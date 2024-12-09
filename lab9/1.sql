6402237b2e4f10a83b4ae85da8dc6b7961f83bb8
Valerii Yakushenko <valerii.yakushenko@gmail.com>
2024-12-09 14:55:23 -0500

    lab9 update

--- lab9/1.sql
+++ lab9/1.sql
@@ -1,34 +1,43 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE projects (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE tasks(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    project_id INT NOT NULL,
+    author_id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE projects(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE files(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    file_path VARCHAR(255) NOT NULL,
+    about_file VARCHAR(255) DEFAULT NULL	
+);
+
+CREATE TABLE tasks_users(
+    user_id INT NOT NULL,
+    task_id INT NOT NULL,
+    PRIMARY KEY (user_id,task_id)
+);
+
+CREATE TABLE projects_users(
+    user_id INT NOT NULL,
+    project_id INT NOT NULL,
+    PRIMARY KEY (user_id,project_id)
+);
+
+CREATE TABLE entity_files(
+    entity_id INT NOT NULL,
+    files_id INT NOT NULL, 
     name VARCHAR(100) NOT NULL,
-    created INT REFERENCES users(id)
-);
-
-CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,
-    project_id INT REFERENCES projects(id),
-    name VARCHAR(100) NOT NULL,
-    author_id INT REFERENCES users(id),
-    assigned_to INT REFERENCES users(id),
-    description TEXT
-);
-
-CREATE TABLE task_users (
-    task_id INT REFERENCES tasks(id),
-    user_id INT REFERENCES users(id), 
-    PRIMARY KEY (task_id, user_id)
-);
-
-CREATE TABLE files (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    project_id INT REFERENCES projects(id),
-    task_id INT REFERENCES tasks(id),
-    link TEXT NOT NULL
+    PRIMARY KEY (entity_id, files_id)
 );
